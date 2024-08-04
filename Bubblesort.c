6df47fb7dbd3490c61efc98a9180ff5fb9d60211
Anubrato Mukherjee <anubrato5@gmail.com>
2024-08-04 19:53:28 +0530

    Update Bubblesort.c

--- Bubblesort.c
+++ Bubblesort.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+void swap(int *xp, int *yp) {
+int temp = *xp;
+*xp = *yp;
+*yp = temp;
+}
 void bubbleSort(int arr[], int n) {
 int i, j;
 for (i = 0; i < n-1; i++) {
@@ -10,7 +14,6 @@ swap(&arr[j], &arr[j+1]);
 }
 }
 }
-
 void printArray(int arr[], int size) {
 int i;
 for (i = 0; i < size; i++) {
